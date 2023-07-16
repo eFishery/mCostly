23578bc58d257efcfc137dd92ef1b91a74753b33
r17x <ri7nz@evilfactory.id>
2023-07-16 19:04:06 +0700

    style: prettify and format all source

--- options.js
+++ options.js
@@ -1,61 +1,70 @@
-const app0 = f => f()
+const app0 = (f) => f();
 
 const defaultOptions = {
   hourlyRate: 560000,
-  currency: 'id-ID'
-}
+  currency: "id-ID",
+};
 
 const id = {
-  hourlyRate: 'optionHourlyRate',
-  currency: 'optionCurrency'
-}
+  hourlyRate: "optionHourlyRate",
+  currency: "optionCurrency",
+};
 
-const setValue = (node, value) => ({
-  [true]: () => { node.value = value },
-  [node.type === 'checkbox']: () => { node.checked = value }
-}).true()
+const setValue = (node, value) =>
+  ({
+    [true]: () => {
+      node.value = value;
+    },
+    [node.type === "checkbox"]: () => {
+      node.checked = value;
+    },
+  }.true());
 
 const setOptionsForCurrency = () => {
-  const currency = document.getElementById(id.currency)
+  const currency = document.getElementById(id.currency);
   const currencies = {
     // TODO
-    IDR: 'id-ID',
-    USD: 'en-US',
-  }
+    IDR: "id-ID",
+    USD: "en-US",
+  };
   Object.entries(currencies).forEach(([key, value]) => {
-    const option = document.createElement('option')
-    option.value = `${key}.${value}`
-    option.append(document.createTextNode(`${key} - ${value}`))
-    currency.append(option)
-  })
-}
+    const option = document.createElement("option");
+    option.value = `${key}.${value}`;
+    option.append(document.createTextNode(`${key} - ${value}`));
+    currency.append(option);
+  });
+};
 
 const restoreOptions = () => {
-  const idElements = Object.entries(id).reduce((acc, [key, id]) => ({
-    ...acc,
-    [key]: document.getElementById(id)
-  }), {})
+  const idElements = Object.entries(id).reduce(
+    (acc, [key, id]) => ({
+      ...acc,
+      [key]: document.getElementById(id),
+    }),
+    {}
+  );
 
-  const setValues = items => Object.entries(idElements).forEach(
-    ([key, node]) => setValue(node, items[key] || defaultOptions[key])
-  )
+  const setValues = (items) =>
+    Object.entries(idElements).forEach(([key, node]) =>
+      setValue(node, items[key] || defaultOptions[key])
+    );
 
-  chrome.storage.sync.get(Object.keys(idElements)).then(setValues)
-}
+  chrome.storage.sync.get(Object.keys(idElements)).then(setValues);
+};
 
-const saveOptions = (options) => chrome.storage.sync.set(options)
+const saveOptions = (options) => chrome.storage.sync.set(options);
 
 const setEventChangeForInput = () => {
   Object.entries(id).forEach(([key, id]) => {
-    const listener = e => {
-      const value = e.currentTarget.value
-      saveOptions({ [key]: value })
-    }
+    const listener = (e) => {
+      const value = e.currentTarget.value;
+      saveOptions({ [key]: value });
+    };
 
-    document.getElementById(id).addEventListener('change', listener)
-  })
-}
+    document.getElementById(id).addEventListener("change", listener);
+  });
+};
 
-document.addEventListener('DOMContentLoaded',
-  () => [setOptionsForCurrency, restoreOptions, setEventChangeForInput].forEach(app0)
-)
+document.addEventListener("DOMContentLoaded", () =>
+  [setOptionsForCurrency, restoreOptions, setEventChangeForInput].forEach(app0)
+);
