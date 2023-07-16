aaf0f17ebcbf279447a1362cf3d6af3d272a94e5
r17x <ri7nz@evilfactory.id>
2023-07-16 19:03:40 +0700

    feat: add participants cost

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 /// <reference path="chrome.d.ts" />
 
-const noOp = () => { };
+const noOp = () => {};
 const nuLL = () => null;
 const d = new Date();
 
@@ -19,16 +19,10 @@ const calculateCost = ({ hourlyRate, durations }) => hourlyRate * durations;
 
 const id = { costly: "mCostly" };
 
-function Message({ currency, ...props }) {
+function Message(props) {
   const element = document.createElement("div");
   const iconWrapper = document.createElement("div");
   const contentWrapper = document.createElement("div");
-  const [_currency, countryCode] = currency.split('.')
-  const formatCurrency = new Intl.NumberFormat(countryCode, {
-    style: 'currency',
-    currency: _currency,
-    currencyDisplay: 'symbol',
-  }).format
 
   iconWrapper.style.paddingLeft = "28px";
   iconWrapper.style.width = "40px";
@@ -38,6 +32,7 @@ function Message({ currency, ...props }) {
   icon.innerHTML = "attach_money";
   iconWrapper.appendChild(icon);
   element.id = id.costly;
+  element.style.width = "100%";
   element.style.display = "flex";
   element.style.alignItems = "center";
   element.style.minHeight = "40px";
@@ -54,7 +49,7 @@ function Message({ currency, ...props }) {
       tag.style.color = "rgb(234,67,53)";
       tag.style.padding = "0 0 0 0.1em";
     }
-    tag.appendChild(document.createTextNode(k === "cost" ? formatCurrency(v) : v));
+    tag.appendChild(document.createTextNode(v));
     contentWrapper.appendChild(tag);
   });
 
@@ -66,51 +61,86 @@ function Message({ currency, ...props }) {
 const getDurations = (timeStr) => {
   const [d1, d2] = (timeStr || "")
     .split("–") // `–` is not equal `-`
-    .map(s => {
-      const re = /(am|pm)/
-      const hasPM = /pm/.test(s)
-      const [h, m] = s.replace(re, '').split(":").map(n => n | 0)
-      return hasPM && h < 12 ? [h + 12, m] : [h, m]
+    .map((s) => {
+      const re = /(am|pm)/;
+      const hasPM = /pm/.test(s);
+      const [h, m] = s
+        .replace(re, "")
+        .split(":")
+        .map((n) => n | 0);
+      return hasPM && h < 12 ? [h + 12, m] : [h, m];
     })
-    .map(toTime)
+    .map(toTime);
 
   return Math.floor((d2 - d1) / (1000 * 60 * 60));
-}
+};
 
-const getOptions = () => chrome.storage.sync.get(['hourlyRate', 'currency'])
+const getOptions = () => chrome.storage.sync.get(["hourlyRate", "currency"]);
 
 function makeViewCostly() {
   // when user click event at calendar.google.com
-  const selector = () => document.querySelector("[data-open-edit-note]")
-  const getNode = () => selector().childNodes[0].childNodes[2]
+  const selector = () => document.querySelector("[data-open-edit-note]");
+  const getNode = () => selector().childNodes[0].childNodes[2];
   const getTimeNode = () =>
-    getNode().childNodes[0].childNodes[1].childNodes[0].childNodes[1].childNodes[2]
-      .firstChild.textContent;
+    getNode().childNodes[0].childNodes[1].childNodes[0].childNodes[1]
+      .childNodes[2].firstChild.textContent;
+  const getParticipantsNode = () =>
+    document.querySelector("[data-show-working-location-actions]")
+      .firstElementChild.childNodes[1].firstChild;
 
   // get options first
   getOptions()
-    .then(options => ({
+    .then((options) => ({
       options,
       node: getNode(),
-      timeNode: getTimeNode()
+      timeNode: getTimeNode(),
+      participantsNode: getParticipantsNode(),
     }))
-    .then(({ options, node, timeNode }) => {
+    .then(({ options, node, timeNode, participantsNode }) => {
+      participantsNode.childNodes.forEach((n) => console.log(n.textContent));
       const durations = getDurations(timeNode);
-      const costly = Message({
-        title: "Estimated Meeting Cost is ",
-        cost: calculateCost({
-          hourlyRate: options.hourlyRate | 0,
-          durations: durations | 0,
-        }),
-        currency: options.currency || 'id-ID'
+      const costEstimation = calculateCost({
+        hourlyRate: options.hourlyRate | 0,
+        durations: durations | 0,
       });
 
-      node.childNodes[1].append(costly);
+      const [currency, countryCode] = options.currency.split(".");
+
+      const formatCurrency = new Intl.NumberFormat(countryCode || "en-US", {
+        style: "currency",
+        currency: currency || "USD",
+        currencyDisplay: "symbol",
+      }).format;
+
+      // formatCurrency_(options.currency.split('.')[1] || "USD")
+
+      const costNode = Message({
+        title: "Estimated Meeting Cost is ",
+        cost: formatCurrency(costEstimation),
+      });
+
+      node.childNodes[1].append(costNode);
+
+      if (participantsNode.firstChild.textContent) {
+        const participants =
+          (participantsNode.firstChild.textContent || "").replace(/\D+/, "") |
+          1;
+        const participantNode = document.createElement("strong");
+        participantNode.style.color = "rgb(234,67,53)";
+        participantNode.textContent = formatCurrency(
+          costEstimation * participants
+        );
+        participantsNode.firstChild.textContent += " - ";
+        participantsNode.firstChild.append(participantNode);
+      }
     })
-    .catch(noOp)
+    .catch(noOp);
 }
 
 const listener = () =>
-  setInterval(() => !document.getElementById(id.costly) && makeViewCostly(), 300);
+  setInterval(
+    () => !document.getElementById(id.costly) && makeViewCostly(),
+    300
+  );
 
-listener()
+listener();
